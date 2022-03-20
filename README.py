1697256d7a7b1866bb3b44f63fdf11d37c655a91
jadenxxx <JadenPark0101@gmail.com>
2022-03-21 06:39:02 +0900

    Update README.py

--- README.py
+++ README.py
@@ -1,2 +1,25 @@
 # mygithub
-print(jaden)
+
+#list[]
+
+subway1=10
+subway2=20
+subway3=30
+
+subway=[10,20,30]
+
+
+sk8=["dime","gx1000","braindead","vans"]
+
+print(sk8.index("dime"))
+
+sk8.append("polarskate")
+
+sk8.insert(0,"palace")
+#insert "palace" at place of 0
+
+print(sk8.pop())
+#take out from tail of list(the result will be"palace",dime","gx1000","braindead","vans")
+
+sk8.append("dime")
+print(sk8.count("dime"))
